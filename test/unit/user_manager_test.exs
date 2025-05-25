8f9c20b66eb5cdc84b8af2f011df59c9dffac7f5
0xSSDD <0xSSDD@protonmail.com>
2025-05-25 16:39:38 +0300

    refactors user_manager_test

--- test/unit/user_manager_test.exs
+++ test/unit/user_manager_test.exs
@@ -3,36 +3,46 @@ defmodule Challenge.UserManagerTest do
 
   setup do
     TestUtils.reset_test_environment()
-    supervisor = TestUtils.start_fresh_challenge()
-
-    on_exit(fn ->
-      TestUtils.stop_challenge(supervisor)
+    |> then(fn _ ->
+      supervisor = TestUtils.start_fresh_challenge()
+      on_exit(fn -> TestUtils.stop_challenge(supervisor) end)
+      %{root_supervisor: supervisor}
     end)
-
-    %{root_supervisor: supervisor}
   end
 
   test "create_users/2 creates users" do
-    assert :ok = Challenge.UserManager.create_users(nil, ["user1", "user2"])
-    assert {:ok, _} = Challenge.UserRegistry.get_user("user1")
-    assert {:ok, _} = Challenge.UserRegistry.get_user("user2")
+    ["user1", "user2"]
+    |> then(&Challenge.UserManager.create_users(nil, &1))
+    |> then(fn result ->
+      assert result == :ok
+      assert {:ok, _} = Challenge.UserRegistry.get_user("user1")
+      assert {:ok, _} = Challenge.UserRegistry.get_user("user2")
+    end)
   end
 
   test "create_users/2 ignores empty and duplicate users" do
-    assert :ok = Challenge.UserManager.create_users(nil, ["", "user1", "user1"])
-    assert {:ok, _} = Challenge.UserRegistry.get_user("user1")
+    ["", "user1", "user1"]
+    |> then(&Challenge.UserManager.create_users(nil, &1))
+    |> then(fn result ->
+      assert result == :ok
+      assert {:ok, _} = Challenge.UserRegistry.get_user("user1")
+    end)
   end
 
   test "get_user_server/1 returns a pid and is idempotent" do
-    Challenge.UserRegistry.create_user("user1")
-    pid1 = Challenge.UserManager.get_user_server("user1")
-    assert is_pid(pid1)
-    pid2 = Challenge.UserManager.get_user_server("user1")
-    assert pid1 == pid2
+    "user1"
+    |> tap(&Challenge.UserRegistry.create_user/1)
+    |> then(&Challenge.UserManager.get_user_server/1)
+    |> then(fn pid1 ->
+      assert is_pid(pid1)
+      pid2 = Challenge.UserManager.get_user_server("user1")
+      assert pid1 == pid2
+    end)
   end
 
   test "get_user_server/1 returns nil for missing user" do
-    assert is_pid(Challenge.UserManager.get_user_server("ghost_user")) or
-             is_nil(Challenge.UserManager.get_user_server("ghost_user"))
+    "ghost_user"
+    |> Challenge.UserManager.get_user_server()
+    |> then(fn pid -> assert is_pid(pid) or is_nil(pid) end)
   end
 end
